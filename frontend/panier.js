511e086853284eb6432141e9b529e549d6ca8fc8
atanassov <atanassov.julien@gmail.com>
2021-07-18 21:32:50 +0200

    v2.3.0
    - requête post fonctionne
    - fonction global pour valider le formulaire

--- frontend/panier.js
+++ frontend/panier.js
@@ -1,43 +1,43 @@
-/* Récupération des produits dans le localStorage */
-let elementLocalStorage = JSON.parse(localStorage.getItem("produit"));
+/* Récupération des produits du panier dans le localStorage */
+let elementLocalStorage = JSON.parse(localStorage.getItem('produit'));
 
 /* Création d'un tableau pour calculer le prix total du panier */
 let prixTotal = [];
 
 /* Gestion du localStorage et insertion dans hmtl */
 if (elementLocalStorage === null) {
-  let elementProduits = document.getElementById("produits");
-  let elementVide = document.createElement("p");
-  elementVide.innerHTML = "Votre panier est vide";
+  let elementProduits = document.getElementById('produits');
+  let elementVide = document.createElement('p');
+  elementVide.innerHTML = 'Votre panier est vide';
   elementProduits.appendChild(elementVide);
 } else {
   /* ------Récupération de chaque produit et insertion du localstorage ------ */
   elementLocalStorage.forEach(element => {
-    let boxCart = document.getElementById("boxCart");
-    let rowProduit = document.createElement("div");
-    rowProduit.className = "row border-top border-bottom";
+    let boxCart = document.getElementById('boxCart');
+    let rowProduit = document.createElement('div');
+    rowProduit.className = 'row border-top border-bottom';
     boxCart.appendChild(rowProduit);
-    let rowAlign = document.createElement("div");
-    rowAlign.className = "row main align-items-center";
+    let rowAlign = document.createElement('div');
+    rowAlign.className = 'row main align-items-center';
     rowProduit.appendChild(rowAlign);
-    let rowCol = document.createElement("div");
-    rowCol.className = "col";
+    let rowCol = document.createElement('div');
+    rowCol.className = 'col';
     rowAlign.appendChild(rowCol);
     /* Nom produit */
-    let colName = document.createElement("div");
-    colName.className = "row";
+    let colName = document.createElement('div');
+    colName.className = 'row';
     colName.innerHTML = element.productName;
     rowCol.appendChild(colName);
     /* Prix produit*/
-    let colPrice = document.createElement("div");
-    colPrice.className = "col";
+    let colPrice = document.createElement('div');
+    colPrice.className = 'col';
     colPrice.innerHTML = element.productPrice;
     rowAlign.appendChild(colPrice);
     /* Bouton supprimer */
-    let suppButton = document.createElement("div");
-    suppButton.className = "btn btn-danger supp";
-    suppButton.type = "button";
-    suppButton.innerHTML = "Supprimer";
+    let suppButton = document.createElement('div');
+    suppButton.className = 'btn btn-danger supp';
+    suppButton.type = 'button';
+    suppButton.innerHTML = 'Supprimer';
     suppButton.dataset.id;
     suppButton.dataset.id = element.productId;
     rowAlign.appendChild(suppButton);
@@ -53,23 +53,26 @@ if (elementLocalStorage === null) {
 /* Prix total */
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 let prixFinal = prixTotal.reduce(reducer);
-let elementPrixTotal = document.getElementById("elementPrixTotal");
+let elementPrixTotal = document.getElementById('elementPrixTotal');
 totalPrice.innerHTML += prixFinal;
 
 /* Bouton supprimer */
 boxCart.onclick = function (evt) {
   /* On selectionne nos boutons */
-  if (evt.target && evt.target.classList.contains("btn-danger")) {
+  if (evt.target && evt.target.classList.contains('btn-danger')) {
     /* On retrouve le data-id */
     const id = evt.target.dataset.id;
-    elementLocalStorage.forEach((element, index) => {
-      const elementString = element.productId.toString();
+    elementLocalStorage.every((element, index) => {
+      const elementString = element.productId;
       /* Si le data-id correspond à un produit du local storage */
       if (elementString === id) {
         /* Le supprimer du localStorage */
         elementLocalStorage.splice(index, 1);
-        localStorage.setItem("produit", JSON.stringify(elementLocalStorage));
-        /* Le supprimer du html */
+        localStorage.setItem('produit', JSON.stringify(elementLocalStorage));
+
+        return false;
+      } else {
+        return true;
       }
     });
     /* Suppression du html du produit */
@@ -79,112 +82,140 @@ boxCart.onclick = function (evt) {
 };
 
 /* Accès au formulaire */
-let formPanier = document.getElementById("panierForm");
+let formPanier = document.getElementById('panierForm');
 
 /*-------- Validation des données du formulaire --------*/
 
 /*--- Nom ---*/
-formPanier.lastName.addEventListener("change", function () {
+formPanier.lastName.addEventListener('change', function () {
   validNames(this);
 });
 /*--- Prénom ---*/
-formPanier.firstName.addEventListener("change", function () {
+formPanier.firstName.addEventListener('change', function () {
   validNames(this);
 });
 /*--- Ville ----*/
-formPanier.city.addEventListener("change", function () {
+formPanier.city.addEventListener('change', function () {
   validNames(this);
 });
 /*--- Adresse ---*/
-formPanier.address.addEventListener("change", function () {
-  validAdress(this);
+formPanier.address.addEventListener('change', function () {
+  validAddress(this);
 });
 /*--- Email ---*/
-formPanier.email.addEventListener("change", function () {
+formPanier.email.addEventListener('change', function () {
   validEmail(this);
 });
 
 /* ---- Fonctions regex afin de valier les input ---- */
 
-/* Fonction  pour valider nom ,prénom et ville */
+/* Fonction pour valider nom ,prénom et ville */
 const validNames = function (inputName) {
   let nameRegExp = /^[a-z ,.'-]+$/i;
   let testName = nameRegExp.test(inputName.value);
   let falseText = inputName.nextElementSibling;
   if (testName) {
-    falseText.innerHTML = "valide";
+    return true;
   } else {
-    falseText.innerHTML = "invalide";
+    falseText.innerHTML = 'Invalide';
+    return false;
   }
 };
-/* Fonction  pour valider l'adresse */
-const validAdress = function (inputAddress) {
-  let addressRegExp = /^([0-9]*) ?([a-zA-Z,\. ]*) ?([0-9]{5}) ?([a-zA-Z]*)/;
+/* Fonction pour valider l'adresse */
+const validAddress = function (inputAddress) {
+  let addressRegExp = /([0-9a-zA-Z,\. ]*) ?([0-9]{5}) ?([a-zA-Z]*)/;
   let testAddress = addressRegExp.test(inputAddress.value);
-  let falseAdress = inputAddress.nextElementSibling;
-  if (testAdress) {
-    falseAdress.innerHTML = "Adresse valide";
+  let falseAddress = inputAddress.nextElementSibling;
+  if (testAddress) {
+    return true;
   } else {
-    falseAdress.innerHTML = "Adresse invalide";
+    falseAddress.innerHTML = 'Invalide';
+    return false;
   }
 };
-/* Fonction  pour valider l'email */
+/* Fonction pour valider l'email */
 const validEmail = function (inputEmail) {
   let emailRegExp =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   let testEmail = emailRegExp.test(inputEmail.value);
   let falseEmail = inputEmail.nextElementSibling;
   if (testEmail) {
-    falseEmail.innerHTML = "Email valide";
+    return true;
   } else {
-    falseEmail.innerHTML = "Email invalide";
+    falseEmail.innerHTML = 'Invalide';
+    return false;
   }
 };
-
+// Fonction pour valider l'ensemble du formulaire
+const validForm = function () {
+  if (validNames(formPanier.lastName)) {
+    if (validNames(formPanier.firstName)) {
+      if (validNames(formPanier.address)) {
+        if (validNames(formPanier.city)) {
+          if (validNames(formPanier.email)) {
+            return true;
+          }
+        }
+      }
+    }
+  } else {
+    return false;
+  }
+};
 /*----------- Passer commande -----------*/
-
-/* Ecoute bouton commander */
-
-/* Récupération des données du formulaire */
-let params = new URL(document.location).searchParams;
-
-let firstName = params.get("firstName");
-let lastName = params.get("lastName");
-let address = params.get("address");
-let city = params.get("city");
-let email = params.get("email");
 
 let products = [];
 elementLocalStorage.forEach(element => {
-  elementString = JSON.stringify(element)
-  products.push(element);
+  product_id = element.productId;
+  products.push(product_id);
 });
-
-console.log(products);
 let contact = {
-  firstName: firstName,
-  lastName: lastName,
-  address: address,
-  city: city,
-  email: email
+  firstName,
+  lastName,
+  address,
+  city,
+  email
 };
 let data = {
-  contact:contact,
-  products:products,
+  contact: contact,
+  products: products
 };
-console.log(JSON.stringify(data));
+
 /*  Envoi des données au backend  */
 function send(e) {
   e.preventDefault();
-  fetch("http://localhost:3000/api/teddies/order", {
-    method: "POST",
-    headers: {
-      'Accept': "application/json",
-      'Content-Type': "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-  
+  validForm();
+  if (validForm() == true) {
+    getValue();
+
+    console.log(data);
+    fetch('http://localhost:3000/api/teddies/order', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(function (res) {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(function (value) {
+        console.log(value);
+      });
+  } else {
+    console.log('erreur formulaire');
+  }
 }
 /* Bouton commander */
-document.getElementById("panierForm").addEventListener("submit", send)
+document.getElementById('panierForm').addEventListener('submit', send);
+/*  Fonction pour récupérer les données du formulaire */
+function getValue() {
+  let lastName = document.getElementById('lastName').value;
+  let firstName = document.getElementById('firstName').value;
+  let address = document.getElementById('address').value;
+  let city = document.getElementById('city').value;
+  let email = document.getElementById('email').value;
+}
--- frontend/produit.js
+++ frontend/produit.js
@@ -1,7 +1,7 @@
 /* Paramétrage du lien en relation avec l'id du produit selectionné */
 let params = new URL(document.location).searchParams;
-let idours = params.get("id");
-let urlId = "http://localhost:3000/api/teddies/" + idours;
+let idours = params.get('id');
+let urlId = 'http://localhost:3000/api/teddies/' + idours;
 
 /* Fonction pour retourner les data du back */
 function fetchData() {
@@ -11,20 +11,26 @@ function fetchData() {
 
 /* Ajout des informations du produit sur la card  */
 fetchData().then(data => {
-  var element = document.getElementById("card-title-id");
+  var element = document.getElementById('card-title-id');
   element.textContent += data.name;
-  var elementImg = document.getElementById("card-img");
+  var elementImg = document.getElementById('card-img');
   elementImg.src = data.imageUrl;
-  var elementDesc = document.getElementById("card-desc");
+  var elementDesc = document.getElementById('card-desc');
   elementDesc.textContent = data.description;
   /* Prix du produit */
   /* Insertion du signe € dans le prix */
   let prixx = data.price;
-  let split = prixx.toString().split("");
-  let prixAffiche = split[0] + split[1] + "€" + split[2] + split[3];
+  let split = prixx.toString().split('');
+  let prixAffiche = split[0] + split[1] + '€' + split[2] + split[3];
   console.log(prixAffiche);
-  let elementPriceBox = document.getElementById("priceElement");
+  let elementPriceBox = document.getElementById('priceElement');
   elementPriceBox.textContent = prixAffiche;
+  /* Liste options couleurs */
+  let colorOptions = data.colors;
+  colorOptions.forEach(function (item) {
+    let elementOptions = document.getElementById('colorOptions');
+    elementOptions.innerHTML += '<option>' + item + '<option>';
+  });
 });
 
 /* Création du tableau contenant les variables des produits */
@@ -35,12 +41,12 @@ let productPrice;
 /* Création de l'objet qui sera envoyé au localStorage */
 
 /* -------   Bouton ajouter au panier  ------- */
-var buttonCart = document.getElementById("bouton");
-buttonCart.addEventListener("click", event => {
+var buttonCart = document.getElementById('bouton');
+buttonCart.addEventListener('click', event => {
   event.preventDefault();
 
   /* Déclaration de la variable où seront stockés keys/values du localStoage */
-  let elementLocalStorage = JSON.parse(localStorage.getItem("produit"));
+  let elementLocalStorage = JSON.parse(localStorage.getItem('produit'));
 
   /* Ajout des valeurs du produit dans le tableau dataProduit */
   fetchData().then(data => {
@@ -50,18 +56,18 @@ buttonCart.addEventListener("click", event => {
     let dataProduit = {
       productName: productName,
       productId: productId,
-      productPrice: productPrice,
+      productPrice: productPrice
     };
     console.log(dataProduit);
 
     /* if:true Si le local storage n'est pas vide */
     if (elementLocalStorage) {
       elementLocalStorage.push(dataProduit);
-      localStorage.setItem("produit", JSON.stringify(elementLocalStorage));
+      localStorage.setItem('produit', JSON.stringify(elementLocalStorage));
     } else {
       elementLocalStorage = [];
       elementLocalStorage.push(dataProduit);
-      localStorage.setItem("produit", JSON.stringify(elementLocalStorage));
+      localStorage.setItem('produit', JSON.stringify(elementLocalStorage));
     }
   });
 });
