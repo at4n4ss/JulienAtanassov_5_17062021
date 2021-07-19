f197042c328a3e658a091a477d58e75b90936b69
atanassov <atanassov.julien@gmail.com>
2021-07-19 03:40:45 +0200

    v2.4.1
    - update prix total apres suppression produit
    - panier vide affichage

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,4 @@
-let url = new URL("http://localhost:3000/api/teddies");
+let url = new URL('http://localhost:3000/api/teddies');
 
 /* 
 async function fetchData() {
@@ -14,49 +14,42 @@ fetch(url)
   .then(response => response.json())
   .then(data => {
     data.forEach(ours => {
-      let oursName = ours.name;
       /* Accès au DOM */
-      const element = document.getElementById("cards");
+      const element = document.getElementById('cards');
       /* Structure du card */
-      var elementBox = document.createElement("div");
-      elementBox.className = "col-md-4";
+      var elementBox = document.createElement('div');
+      elementBox.className = 'col-md-4';
       element.appendChild(elementBox);
-
-      const elementTitle = document.createElement("div");
-      elementTitle.className = "card";
+      const elementTitle = document.createElement('div');
+      elementTitle.className = 'card';
       elementBox.appendChild(elementTitle);
-
       /* Image du produit */
-      const elementImg = document.createElement("img");
+      const elementImg = document.createElement('img');
       var oursImg = ours.imageUrl;
       elementImg.src = oursImg;
-      elementImg.className = "card-img-top";
+      elementImg.className = 'card-img-top';
       elementTitle.appendChild(elementImg);
       /* Structure du card */
-      const elementCardBody = document.createElement("div");
-      elementCardBody.className = "card-body";
+      const elementCardBody = document.createElement('div');
+      elementCardBody.className = 'card-body';
       elementTitle.appendChild(elementCardBody);
       /* Nom du produit */
-      const elementText = document.createElement("h5");
-      elementText.className = "card-text";
+      const elementText = document.createElement('h5');
+      elementText.className = 'card-text';
       elementText.innerHTML = ours.name;
       elementCardBody.appendChild(elementText);
       /* Prix du produit */
-      /* Insertion du signe € dans le prix */
       let prixx = ours.price;
-      let split = prixx.toString().split("");
-      let prixAffiche = split[0] + split[1] + "€" + split[2] + split[3];
-      console.log(prixAffiche);
-      const elementPrice = document.createElement("p");
-      elementPrice.className = "card-text";
-      elementPrice.innerHTML = prixAffiche;
+      const elementPrice = document.createElement('p');
+      elementPrice.className = 'card-text';
+      elementPrice.innerHTML = prixx;
       elementCardBody.appendChild(elementPrice);
       /* Bouton acheter et lien ID */
       let elementID = ours._id;
-      const elementButton = document.createElement("a");
-      elementButton.className = "btn btn-primary";
-      elementButton.href = "produit.html?id=" + elementID;
-      elementButton.innerHTML = "Description du produit";
+      const elementButton = document.createElement('a');
+      elementButton.className = 'btn btn-primary';
+      elementButton.href = 'produit.html?id=' + elementID;
+      elementButton.innerHTML = 'Description du produit';
       elementCardBody.appendChild(elementButton);
       /* Lien enveloppant le card */
 
--- frontend/panier.js
+++ frontend/panier.js
@@ -1,66 +1,76 @@
 /* Récupération des produits du panier dans le localStorage */
-let elementLocalStorage = JSON.parse(localStorage.getItem('produit'));
+let elementLocalStorage = JSON.parse(localStorage.getItem("produit"));
 
 /* Création d'un tableau pour calculer le prix total du panier */
 let prixTotal = [];
-
-/* Gestion du localStorage et insertion dans hmtl */
-if (elementLocalStorage === null) {
-  let elementProduits = document.getElementById('produits');
-  let elementVide = document.createElement('p');
-  elementVide.innerHTML = 'Votre panier est vide';
+/* Informer le client que son panier est vide */
+const panierVide = function () {
+  let elementProduits = document.getElementById("boxCart");
+  let elementVide = document.createElement("p");
+  elementVide.innerHTML = "Votre panier est vide";
   elementProduits.appendChild(elementVide);
-} else {
-  /* ------Récupération de chaque produit et insertion du localstorage ------ */
-  elementLocalStorage.forEach(element => {
-    let boxCart = document.getElementById('boxCart');
-    let rowProduit = document.createElement('div');
-    rowProduit.className = 'row border-top border-bottom';
-    boxCart.appendChild(rowProduit);
-    let rowAlign = document.createElement('div');
-    rowAlign.className = 'row main align-items-center';
-    rowProduit.appendChild(rowAlign);
-    let rowCol = document.createElement('div');
-    rowCol.className = 'col';
-    rowAlign.appendChild(rowCol);
-    /* Nom produit */
-    let colName = document.createElement('div');
-    colName.className = 'row';
-    colName.innerHTML = element.productName;
-    rowCol.appendChild(colName);
-    /* Prix produit*/
-    let colPrice = document.createElement('div');
-    colPrice.className = 'col';
-    colPrice.innerHTML = element.productPrice;
-    rowAlign.appendChild(colPrice);
-    /* Bouton supprimer */
-    let suppButton = document.createElement('div');
-    suppButton.className = 'btn btn-danger supp';
-    suppButton.type = 'button';
-    suppButton.innerHTML = 'Supprimer';
-    suppButton.dataset.id;
-    suppButton.dataset.id = element.productId;
-    rowAlign.appendChild(suppButton);
-    /* Total price */
-    let prixx = element.productPrice;
-
-    prixFloat = parseFloat(prixx);
-    prixTotal.push(prixFloat);
-    produits = elementLocalStorage;
-  });
-}
+};
+/* Gestion du localStorage et insertion dans hmtl */
+const panierContent = function () {
+  if (elementLocalStorage.length === 0) {
+    panierVide();
+  } else {
+    /* ------Récupération de chaque produit et insertion du localstorage ------ */
+    elementLocalStorage.forEach(element => {
+      let boxCart = document.getElementById("boxCart");
+      let rowProduit = document.createElement("div");
+      rowProduit.className = "row border-top border-bottom";
+      boxCart.appendChild(rowProduit);
+      let rowAlign = document.createElement("div");
+      rowAlign.className = "row main align-items-center";
+      rowProduit.appendChild(rowAlign);
+      let rowCol = document.createElement("div");
+      rowCol.className = "col";
+      rowAlign.appendChild(rowCol);
+      /* Nom produit */
+      let colName = document.createElement("div");
+      colName.className = "row";
+      colName.innerHTML = element.productName;
+      rowCol.appendChild(colName);
+      /* Prix produit*/
+      let colPrice = document.createElement("div");
+      colPrice.className = "col";
+      colPrice.innerHTML = element.productPrice;
+      rowAlign.appendChild(colPrice);
+      /* Bouton supprimer */
+      let suppButton = document.createElement("div");
+      suppButton.className = "btn btn-danger supp";
+      suppButton.type = "button";
+      suppButton.innerHTML = "Supprimer";
+      suppButton.dataset.id;
+      suppButton.dataset.id = element.productId;
+      rowAlign.appendChild(suppButton);
+    });
+  }
+};
+panierContent();
 
 /* Prix total */
-const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
 const totalCart = function () {
-  let prixFinal = prixTotal.reduce(reducer);
-  totalPrice.innerHTML = prixFinal;
+  if (elementLocalStorage.length === 0) {
+    totalPrice.innerHTML = "0";
+  } else {
+    prixTotal.splice(0, prixTotal.length);
+    elementLocalStorage.forEach(element => {
+      let prixx = element.productPrice;
+      prixTotal.push(prixx);
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      let prixFinal = prixTotal.reduce(reducer);
+      totalPrice.innerHTML = prixFinal;
+    });
+  }
 };
 totalCart();
 /* Bouton supprimer */
 boxCart.onclick = function (evt) {
   /* On selectionne nos boutons */
-  if (evt.target && evt.target.classList.contains('btn-danger')) {
+  if (evt.target && evt.target.classList.contains("btn-danger")) {
     /* On retrouve le data-id */
     const id = evt.target.dataset.id;
     elementLocalStorage.every((element, index) => {
@@ -69,7 +79,7 @@ boxCart.onclick = function (evt) {
       if (elementString === id) {
         /* Le supprimer du localStorage */
         elementLocalStorage.splice(index, 1);
-        localStorage.setItem('produit', JSON.stringify(elementLocalStorage));
+        localStorage.setItem("produit", JSON.stringify(elementLocalStorage));
         return false;
       } else {
         return true;
@@ -79,31 +89,32 @@ boxCart.onclick = function (evt) {
     let evtButton = evt.target;
     evtButton.parentElement.parentElement.remove();
   }
+  totalCart();
 };
 
 /* Accès au formulaire */
-let formPanier = document.getElementById('panierForm');
+let formPanier = document.getElementById("panierForm");
 
 /*-------- Validation des données du formulaire --------*/
 
 /*--- Nom ---*/
-formPanier.lastName.addEventListener('change', function () {
+formPanier.lastName.addEventListener("change", function () {
   validNames(this);
 });
 /*--- Prénom ---*/
-formPanier.firstName.addEventListener('change', function () {
+formPanier.firstName.addEventListener("change", function () {
   validNames(this);
 });
 /*--- Ville ----*/
-formPanier.city.addEventListener('change', function () {
+formPanier.city.addEventListener("change", function () {
   validNames(this);
 });
 /*--- Adresse ---*/
-formPanier.address.addEventListener('change', function () {
+formPanier.address.addEventListener("change", function () {
   validAddress(this);
 });
 /*--- Email ---*/
-formPanier.email.addEventListener('change', function () {
+formPanier.email.addEventListener("change", function () {
   validEmail(this);
 });
 
@@ -115,10 +126,10 @@ const validNames = function (inputName) {
   let testName = nameRegExp.test(inputName.value);
   let falseText = inputName.nextElementSibling;
   if (testName) {
-    falseText.innerHTML = '';
+    falseText.innerHTML = "";
     return true;
   } else {
-    falseText.innerHTML = 'Invalide';
+    falseText.innerHTML = "Invalide";
     return false;
   }
 };
@@ -128,10 +139,10 @@ const validAddress = function (inputAddress) {
   let testAddress = addressRegExp.test(inputAddress.value);
   let falseAddress = inputAddress.nextElementSibling;
   if (testAddress) {
-    falseAddress.innerHTML = '';
+    falseAddress.innerHTML = "";
     return true;
   } else {
-    falseAddress.innerHTML = 'Invalide';
+    falseAddress.innerHTML = "Invalide";
     return false;
   }
 };
@@ -142,10 +153,10 @@ const validEmail = function (inputEmail) {
   let testEmail = emailRegExp.test(inputEmail.value);
   let falseEmail = inputEmail.nextElementSibling;
   if (testEmail) {
-    falseEmail.innerHTML = '';
+    falseEmail.innerHTML = "";
     return true;
   } else {
-    falseEmail.innerHTML = 'Invalide';
+    falseEmail.innerHTML = "Invalide";
     return false;
   }
 };
@@ -184,19 +195,26 @@ let data = {
   products: products
 };
 
-/*  Envoi des données au backend  */
+/* ----- Envoi des données au backend ----- */
+/*  Fonction pour récupérer les données du formulaire */
+function getValue() {
+  let lastName = document.getElementById("lastName").value;
+  let firstName = document.getElementById("firstName").value;
+  let address = document.getElementById("address").value;
+  let city = document.getElementById("city").value;
+  let email = document.getElementById("email").value;
+}
 function send(e) {
   e.preventDefault();
   validForm();
   if (validForm() == true) {
     getValue();
 
-    console.log(data);
-    fetch('http://localhost:3000/api/teddies/order', {
-      method: 'POST',
+    fetch("http://localhost:3000/api/teddies/order", {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        "Accept": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify(data)
     })
@@ -209,16 +227,8 @@ function send(e) {
         console.log(value);
       });
   } else {
-    console.log('erreur formulaire');
+    console.log("erreur formulaire");
   }
 }
 /* Bouton commander */
-document.getElementById('panierForm').addEventListener('submit', send);
-/*  Fonction pour récupérer les données du formulaire */
-function getValue() {
-  let lastName = document.getElementById('lastName').value;
-  let firstName = document.getElementById('firstName').value;
-  let address = document.getElementById('address').value;
-  let city = document.getElementById('city').value;
-  let email = document.getElementById('email').value;
-}
+document.getElementById("panierForm").addEventListener("submit", send);
